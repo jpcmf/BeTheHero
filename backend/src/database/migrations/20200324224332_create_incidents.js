8ad73c0b09469514cbbb72116462f1684a8b1665
João Paulo <musicjoaopaulo@gmail.com>
2020-03-24 23:36:25 -0300

    chore(backend): create the incidents controller

--- backend/src/database/migrations/20200324224332_create_incidents.js
+++ backend/src/database/migrations/20200324224332_create_incidents.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('incidents', function(table) {
     table.increments();
     table.string('title').notNullable();
-    table.string('descriptiom').notNullable();
+    table.string('description').notNullable();
     table.decimal('value').notNullable();
 
     table.string('ong_id').notNullable();
